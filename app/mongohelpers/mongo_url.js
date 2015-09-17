749ac75ce81d2d01b40c2390c9bd26913aec2b14
valgaze <valgaze@gmail.com>
2015-09-16 20:55:48 -0700

    Used mongohelpers

---
+++ app/mongohelpers/mongo_url.js
@@ -0,0 +1,31 @@
+var db = require('../config');
+var crypto = require('crypto');
+var Schema = db.Schema;
+
+  var urlSchema = Schema({
+    url: String,
+    base_url: String,
+    code: String,
+    title: String,
+    visits: {Number, default: 0},
+    timestamp: Date.now
+  });
+
+  urlSchema.methods.encodeLink = function(){
+    var shasum = crypto.createHash('sha1');
+      shasum.update(this.url);
+      this.code = shasum.digest('hex').slice(0, 5));
+  }
+
+  var Url = db.model('url', urlSchema);
+
+  module.exports = Url;
+
+  //var aLink = new Url({url:"http://www.google.com"}); 
+    //Before saving:
+    //1) Check if a valid URL (utility library)
+    //2) Check if it already exists in the database (query, make a proper mongo object for querying)
+        //If all good, retrieve the title (utility library)
+          //When that's done with a callback, then attach the base_url
+          //Then save it to the database
+            //If no error, get the data and send it back to the user!
---
+++ app/mongohelpers/mongo_user.js
@@ -0,0 +1,39 @@
+var db = require('../config');
+var bcrypt = require('bcrypt-nodejs');
+var Schema = db.Schema;
+
+  var userSchema = Schema({
+    username: String,
+    password: String,
+    timestamp: Date.now
+  });
+
+  userSchema.methods.hashPassword = function(){
+    var salt = bcrypt.genSaltSync(10);
+    var password = bcrypt.hashSync(this.password, salt);
+    this.password = password;
+  }
+
+  var User = db.model('User', userSchema);
+
+  module.exports = User;
+
+/**** USAGE:
+  //Instantiate:
+  var aUser = new User({username:"myUser123", password:""})
+  //Hashpassword
+  aUser.hashPassword()// {username:"myUser123", password: "asdfihasdjkfjasld"}
+
+  
+  //For login: you don't need to store the salt with bcrypt's implementation. This means 
+  // you cannot use ===, but use their compare function will hash the input and return true/false in a callback
+  var password = bcrypt.hashSync("apples123", salt);
+
+    bcrypt.compare("apples123", '$2a$10$hMxvvhXRyjixsZPZjORjdOEbaatg99ztk/OOd4fcqAqrphLUhys/m', function(err, res) {
+    console.log(res);
+       // If res is true, then log them in and do all the rest
+
+       // If res is false
+    })
+
+****/
